a6c7f4ef64f60b545dd5fe7c9c4ebbef1c3d94ce
Yuri <yuri_s.c@outlook.com>
2023-04-26 19:33:43 -0300

    Add pág de estações

--- route_search/lib/view/stations_page.dart
+++ route_search/lib/view/stations_page.dart
@@ -1,12 +1,66 @@
 import 'package:flutter/material.dart';
 
-class StationsPage extends StatelessWidget {
+class StationsPage extends StatefulWidget {
   const StationsPage({super.key});
 
   @override
+  State<StationsPage> createState() => _StationsPageState();
+}
+
+class _StationsPageState extends State<StationsPage> {
+  final stations = [
+    'Heloísa Marcela',
+    'Jairo Messias',
+    'Juca Ramos',
+    'Leonardo Amarelo',
+    'Zayon Rodrigues'
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Station Screen'),
+    return Column(children: [
+      _handleListView(context),
+      Padding(
+          padding: EdgeInsets.only(bottom: 16.0),
+          child: FloatingActionButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/registro');
+            },
+            child: Icon(Icons.add),
+            backgroundColor: Colors.blue,
+          )),
+    ]);
+  }
+
+  deleteStation(int id) {
+    setState(() {
+      stations.remove(stations[id]);
+    });
+  }
+
+  Widget _handleListView(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: stations.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+              title: Text(stations[index]),
+              trailing: Row(mainAxisSize: MainAxisSize.min, children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/registro');
+                  },
+                  icon: const Icon(Icons.edit),
+                ),
+                IconButton(
+                  onPressed: () {
+                    deleteStation(index);
+                  },
+                  icon: const Icon(Icons.delete),
+                ),
+              ]));
+        },
+      ),
     );
   }
 }
