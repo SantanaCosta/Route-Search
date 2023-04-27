053d99d87dd1ff5983fedfaad927c387fc1b289b
Yuri <yuri_s.c@outlook.com>
2023-04-26 23:53:06 -0300

    add cadastro

--- route_search/lib/view/station_register_page.dart
+++ route_search/lib/view/station_register_page.dart
@@ -14,11 +14,125 @@ class _StationRegisterPageState extends State<StationRegisterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(''),
+          title: const Text('Registro'),
         ),
-        body: const Center(child: Text(''),)
-    );
-      
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              _handleTextFields(context),
+              Text('Conexões: '),
+              _handleConnectionsList(context),
+              _handleSaveButton(context)
+            ],
+          ),
+        ));
   }
-  
+
+  bool isSwitched = false;
+
+  Widget _handleTextFields(BuildContext context) {
+    return Column(
+      children: [
+        const TextField(
+            decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Nome da estação',
+        )),
+        Row(
+          children: [
+            const SizedBox(
+              width: 75,
+              child: TextField(
+                  decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'X',
+              )),
+            ),
+            const SizedBox(height: 70, width: 10),
+            const SizedBox(
+              width: 75,
+              child: TextField(
+                  decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Y',
+              )),
+            ),
+            Switch(
+                value: isSwitched,
+                onChanged: (bool newValue) {
+                  setState(() {
+                    isSwitched = newValue;
+                  });
+                })
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _handleSaveButton(BuildContext context) {
+    return Container(
+        color: Colors.green,
+        margin: EdgeInsets.all(10),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.8,
+          child: ElevatedButton(
+            child: Text('SALVAR'),
+            onPressed: () {
+              //
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.green,
+              minimumSize: Size(0, 50),
+            ),
+          ),
+        ));
+  }
+
+  final stations = [
+    'Heloísa Marcela',
+    'Jairo Messias',
+    'Juca Ramos',
+    'Leonardo Amarelo',
+    'Zayon Rodrigues'
+  ];
+
+  int line = -1;
+  int row = -1;
+
+  void changeButtonColour(int index, int icon) {
+    setState(() {
+      line = index;
+      row = icon;
+    });
+  }
+
+  Widget _handleConnectionsList(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: stations.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+              title: Text(stations[index]),
+              trailing: Row(mainAxisSize: MainAxisSize.min, children: [
+                IconButton(
+                    onPressed: () {
+                      changeButtonColour(index, 1);
+                    },
+                    icon: const Icon(Icons.check_circle),
+                    color:
+                        index == line && row == 1 ? Colors.green : Colors.grey),
+                IconButton(
+                    onPressed: () {
+                      changeButtonColour(index, 2);
+                    },
+                    icon: const Icon(Icons.offline_bolt),
+                    color:
+                        index == line && row == 2 ? Colors.amber : Colors.grey),
+              ]));
+        },
+      ),
+    );
+  }
 }
